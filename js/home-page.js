d0a73ec436280a067ab0315b8be9768e1bf40186
mjaumi <mjaumi2864@gmail.com>
2022-01-28 18:33:53 +0600

    header fixed

--- js/home-page.js
+++ js/home-page.js
@@ -3,9 +3,46 @@ var hamBurger = document.getElementById('nav-hamburger-button');
 
 var navItems = document.getElementsByClassName('austhir-nav-link');
 
-console.log(navItems);
+var lastScrollTop = 0;
 
-hamBurger.onclick = function(){
+hamBurger.onclick = function () {
 
-    navBar.classList.toggle('nav-bg'); 
+    navBar.classList.toggle('nav-bg');
+    for (var i = 0; i < navItems.length; i++) {
+
+        navItems.item(i).classList.add('fixed');
+    }
+}
+
+window.onscroll = function () {
+
+    var st = window.pageYOffset || document.documentElement.scrollTop;
+    if (st > lastScrollTop) {
+
+        if (window.screen.width > 990) {
+            navBar.classList.add('nav-bg');
+            navBar.classList.add('fixed');
+
+            for (var i = 0; i < navItems.length; i++) {
+
+                navItems.item(i).classList.add('fixed');
+            }
+        }
+
+
+
+    } else {
+
+        if (st === 0 && window.screen.width > 990) {
+            navBar.classList.remove('nav-bg');
+            navBar.classList.remove('fixed');
+
+            for (var i = 0; i < navItems.length; i++) {
+
+                navItems.item(i).classList.remove('fixed');
+            }
+        }
+
+    }
+    lastScrollTop = st <= 0 ? 0 : st;
 }
