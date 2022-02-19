2427ccafa5b363a307feab63be4bac884fb6066f
azizTarik-ui <69195041+azizTarik-ui@users.noreply.github.com>
2022-02-19 18:14:44 +0600

    config

--- config.php
+++ config.php
@@ -1,8 +1,6 @@
 <?php
 
-$server = "DESKTOP-3R52OF2\\SQLEXPRESS";
-$connectionInfo = array("Database"=>"AusthirCars", "user"=>"sa", "password"=>"mypassforsql");
-$conn = sqlsrv_connect($server, $connectionInfo);
+
 
 
 
