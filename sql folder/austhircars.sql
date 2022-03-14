bf7e06f878b568dcfa46eff4d0999dcd6b9cc3f9
salsabeelnoor <salsabeelnoor@gmail.com>
2022-03-15 01:40:57 +0600

    sql updated

--- sql folder/austhircars.sql
+++ sql folder/austhircars.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 04, 2022 at 12:45 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 7.4.23
+-- Generation Time: Mar 14, 2022 at 08:38 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,6 +33,13 @@ CREATE TABLE `admininfo` (
   `adminPassword` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `admininfo`
+--
+
+INSERT INTO `admininfo` (`adminId`, `adminName`, `adminPassword`) VALUES
+('1', 'admin.azmi@austhircars.com', '81dc9bdb52d04dc20036dbd8313ed055');
+
 -- --------------------------------------------------------
 
 --
@@ -44,7 +51,7 @@ CREATE TABLE `cardetails` (
   `carId` varchar(50) NOT NULL,
   `maker` varchar(50) NOT NULL,
   `transmission` varchar(50) NOT NULL,
-  `carCondition` varchar(50) NOT NULL,
+  `carCondition` varchar(50) NOT NULL DEFAULT 'New',
   `engineSize` varchar(50) DEFAULT NULL,
   `doors` varchar(50) NOT NULL,
   `cylinder` varchar(50) NOT NULL,
@@ -52,7 +59,7 @@ CREATE TABLE `cardetails` (
   `fuelType` varchar(50) NOT NULL,
   `driveType` varchar(100) NOT NULL DEFAULT 'Front Wheel Drive',
   `milage` varchar(50) DEFAULT NULL,
-  `description` text DEFAULT NULL,
+  `descriptions` text DEFAULT NULL,
   `videoLink` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -60,25 +67,25 @@ CREATE TABLE `cardetails` (
 -- Dumping data for table `cardetails`
 --
 
-INSERT INTO `cardetails` (`id`, `carId`, `maker`, `transmission`, `carCondition`, `engineSize`, `doors`, `cylinder`, `color`, `fuelType`, `driveType`, `milage`, `description`, `videoLink`) VALUES
-(1, 'AD0012', 'Audi', 'Autometic', 'Used', '4.0L', '5-Door', '6', 'Silver', 'Petrol', 'Front Wheel Drive', NULL, NULL, NULL),
-(2, 'AD0015', 'Audi', 'Semi-Autometic', 'New', '3.2L', '5-Door', '6', 'Red', 'Diesel', 'Front Wheel Drive', '245,000 miles', NULL, NULL),
-(3, 'AD0018', 'Audi', 'Autometic', 'Used', NULL, '5-Door', '4', 'Blue', 'Electric', 'Front Wheel Drive', '170,000 miles', NULL, NULL),
-(4, 'AD004', 'Audi', 'Manual', 'Used', '2.0L', '4-Door', '6', 'Blue', 'Diesel', 'Front Wheel Drive', '245,000 miles', NULL, NULL),
-(5, 'AD006', 'Audi', 'Autometic', 'New', '4.8L', '4-Door', '8', 'Silver', 'Diesel', 'Front Wheel Drive', NULL, NULL, NULL),
-(6, 'B009', 'Bentley', 'Manual', 'Used', '5.0L', '4-Door', '8', 'Blue', 'Petrol', 'Front Wheel Drive', '210,000 miles', NULL, NULL),
-(7, 'BMW001', 'BMW', 'Autometic', 'New', '3.8L', '2-Door', '4', 'Grey', 'Petrol', 'Front Wheel Drive', NULL, NULL, NULL),
-(8, 'BMW0013', 'BMW', 'Manual', 'New', NULL, '5-Door', '6', 'Silver', 'Electric', 'Front Wheel Drive', NULL, NULL, NULL),
-(9, 'BMW0016', 'BMW', 'Manual', 'New', '4.0L', '5-Door', '8', 'Black', 'Hybrid', 'Front Wheel Drive', NULL, NULL, NULL),
-(10, 'C0010', 'Cadillac', 'Manual', 'New', '3.6L', '4-Door', '8', 'White', 'Petrol', 'Front Wheel Drive', NULL, NULL, NULL),
-(11, 'F005', 'Ferrari', 'Autometic', 'New', '4.8L', '2-Door', '8', 'Red', 'Diesel', 'Front Wheel Drive', NULL, NULL, NULL),
-(12, 'FR007', 'Ford', 'Manual', 'Used', '4.8L', '2-Door', '6', 'Red', 'Diesel', 'Front Wheel Drive', '280,000 miles', NULL, NULL),
-(13, 'MB0014', 'Mercedes-Benz', 'Autometic', 'Used', '4.2L', '5-Door', '4', 'Red', 'Petrol', 'Front Wheel Drive', '360,000 miles', NULL, NULL),
-(14, 'MB0017', 'Mercedes-Benz', 'Semi-Autometic', 'Used', '3.8L', '4-Door', '8', 'White', 'Diesel', 'Front Wheel Drive', '140,000 miles', NULL, NULL),
-(15, 'MB002', 'Mercedes-Benz', 'Autometic', 'Used', '4.8L', '2-Door', '8', 'Yellow', 'Diesel', 'Front Wheel Drive', '160,000 miles', NULL, NULL),
-(16, 'MB008', 'Mercedes-Benz', 'Autometic', 'Used', NULL, '5-Door', '6', 'White', 'Electric', 'Front Wheel Drive', '7,000 miles', NULL, NULL),
-(17, 'PC0011', 'Porsche', 'Semi-Autometic', 'New', '2.8L', '5-Door', '8', 'White', 'Diesel', 'Front Wheel Drive', '200,000 miles', NULL, NULL),
-(18, 'PC003', 'Porsche', 'Autometic', 'Used', '4.8L', '5-Door', '6', 'Blue', 'Diesel', 'Front Wheel Drive', '190,000 miles', NULL, NULL);
+INSERT INTO `cardetails` (`id`, `carId`, `maker`, `transmission`, `carCondition`, `engineSize`, `doors`, `cylinder`, `color`, `fuelType`, `driveType`, `milage`, `descriptions`, `videoLink`) VALUES
+(1, 'AD005', 'Audi', 'Automatic', 'Used', '4.0L', '5-Door', '6', 'Silver', 'Petrol', 'AWD/4WD', NULL, NULL, NULL),
+(2, 'AD004', 'Audi', 'Semi-Automatic', 'New', '3.2L', '5-Door', '6', 'Red', 'Diesel', 'Front Wheel Drive', '245,000 miles', NULL, NULL),
+(3, 'AD003', 'Audi', 'Automatic', 'Used', NULL, '5-Door', '4', 'Blue', 'Electric', 'Front Wheel Drive', '170,000 miles', NULL, NULL),
+(4, 'AD002', 'Audi', 'Manual', 'Used', '2.0L', '4-Door', '6', 'Blue', 'Diesel', 'Front Wheel Drive', '245,000 miles', NULL, 'https://www.youtube.com/embed/fPoTCZopqjU'),
+(5, 'AD001', 'Audi', 'Automatic', 'New', '4.8L', '4-Door', '8', 'Silver', 'Diesel', 'Front Wheel Drive', NULL, NULL, NULL),
+(6, 'B001', 'Bentley', 'Manual', 'Used', '5.0L', '4-Door', '8', 'Blue', 'Petrol', 'Front Wheel Drive', '210,000 miles', NULL, NULL),
+(7, 'BMW001', 'BMW', 'Automatic', 'New', '3.8L', '2-Door', '4', 'Grey', 'Petrol', 'Front Wheel Drive', NULL, NULL, NULL),
+(8, 'BMW002', 'BMW', 'Manual', 'New', NULL, '5-Door', '6', 'Silver', 'Electric', 'Front Wheel Drive', NULL, NULL, NULL),
+(9, 'BMW003', 'BMW', 'Manual', 'New', '4.0L', '5-Door', '8', 'Black', 'Hybrid', 'Front Wheel Drive', NULL, NULL, NULL),
+(10, 'C001', 'Cadillac', 'Manual', 'New', '3.6L', '4-Door', '8', 'White', 'Petrol', 'Front Wheel Drive', NULL, NULL, NULL),
+(11, 'F001', 'Ferrari', 'Automatic', 'New', '4.8L', '2-Door', '8', 'Red', 'Diesel', 'Front Wheel Drive', NULL, NULL, NULL),
+(12, 'FR001', 'Ford', 'Manual', 'Used', '4.8L', '2-Door', '6', 'Red', 'Diesel', 'Front Wheel Drive', '280,000 miles', NULL, NULL),
+(13, 'MB001', 'Mercedes-Benz', 'Autometic', 'Used', '4.2L', '5-Door', '4', 'Red', 'Petrol', 'Front Wheel Drive', '360,000 miles', NULL, NULL),
+(14, 'MB003', 'Mercedes-Benz', 'Semi-Automatic', 'Used', '3.8L', '4-Door', '8', 'White', 'Diesel', 'Front Wheel Drive', '140,000 miles', NULL, NULL),
+(15, 'MB002', 'Mercedes-Benz', 'Automatic', 'Used', '4.5L', '2-Door', '8', 'Yellow', 'Diesel', 'Front Wheel Drive', '160,000 miles', NULL, NULL),
+(16, 'MB004', 'Mercedes-Benz', 'Automatic', 'Used', NULL, '5-Door', '6', 'White', 'Electric', 'Front Wheel Drive', '7,000 miles', NULL, NULL),
+(17, 'PC001', 'Porsche', 'Semi-Automatic', 'New', '2.8L', '5-Door', '8', 'White', 'Diesel', 'Front Wheel Drive', '200,000 miles', NULL, NULL),
+(18, 'PC002', 'Porsche', 'Automatic', 'Used', '4.8L', '5-Door', '6', 'Blue', 'Diesel', 'Front Wheel Drive', '190,000 miles', NULL, 'https://www.youtube.com/embed/fPoTCZopqjU');
 
 -- --------------------------------------------------------
 
@@ -100,7 +107,7 @@ CREATE TABLE `cargallary` (
 
 CREATE TABLE `cars` (
   `carId` varchar(50) NOT NULL,
-  `carName` varchar(255) DEFAULT NULL,
+  `carTittle` varchar(255) DEFAULT NULL,
   `carBrand` varchar(50) NOT NULL,
   `carModel` varchar(255) NOT NULL,
   `carPrice` varchar(50) NOT NULL,
@@ -115,25 +122,45 @@ CREATE TABLE `cars` (
 -- Dumping data for table `cars`
 --
 
-INSERT INTO `cars` (`carId`, `carName`, `carBrand`, `carModel`, `carPrice`, `carEMI`, `carGenre`, `carReleaseDate`, `carSeats`, `carImage`) VALUES
-('AD0012', NULL, 'Audi', 'A7', '5000000', 'Available', 'Hatchback', '2020', 5, 'audia7.jpg'),
-('AD0015', NULL, 'Audi ', 'A4 Avant', '8500000', 'Available', 'Wagon', '2021', 5, 'audia4avant.jpg'),
-('AD0018', NULL, 'Audi ', 'e-tron', '15000000', 'Available', 'SUV', '2018', 6, 'audietron.jpg'),
-('AD004', NULL, 'Audi', 'A4', '7000000', 'Available', 'Sedan', '2020', 4, 'audia4.jpg'),
-('AD006', NULL, 'Audi', 'A8', '15000000', 'Available', 'Sedan', '2021', 4, 'audia8.jpg'),
-('B009', NULL, 'Bentley', 'Mulsanne', '3500000', 'Available', 'Sedan', '2017', 4, 'bentleymulsanne.jpg'),
+INSERT INTO `cars` (`carId`, `carTittle`, `carBrand`, `carModel`, `carPrice`, `carEMI`, `carGenre`, `carReleaseDate`, `carSeats`, `carImage`) VALUES
+('AD001', NULL, 'Audi', 'A8', '15000000', 'Available', 'Sedan', '2021', 4, 'audia8.jpg'),
+('AD002', NULL, 'Audi', 'A4', '7000000', 'Available', 'Sedan', '2020', 4, 'audia4.jpg'),
+('AD003', NULL, 'Audi ', 'e-tron', '15000000', 'Available', 'SUV', '2018', 6, 'audietron.jpg'),
+('AD004', NULL, 'Audi ', 'A4 Avant', '8500000', 'Available', 'Wagon', '2021', 5, 'audia4avant.jpg'),
+('AD005', NULL, 'Audi', 'A7', '5000000', 'Available', 'Hatchback', '2020', 5, 'audia7.jpg'),
+('B001', NULL, 'Bentley', 'Mulsanne', '3500000', 'Available', 'Sedan', '2017', 4, 'bentleymulsanne.jpg'),
 ('BMW001', NULL, 'BMW', '8-Serie', '24000000', 'Available', 'Coupe', '2021', 2, 'bmw8serie.jpg'),
-('BMW0013', NULL, 'BMW', '4-serie', '5305000', 'Available', 'Hatchback', '2021', 4, 'bmw4serie.jpg'),
-('BMW0016', NULL, 'BMW', 'X7', '5400000', 'Available', 'SUV', '2021', 6, 'bmwx7.jpg'),
-('C0010', NULL, 'Cadillac', 'CT6', '9000000', 'Available', 'Sedan', '2021', 4, 'cadillacct6.jpg'),
-('F005', NULL, 'Ferrari', 'LaFerrari', '200000000', 'Available', 'Coupe', '2021', 2, 'ferarilaferari.jpg'),
-('FR007', NULL, 'Ford', 'Mustang', '12000000', 'Available', 'Coupe', '2018', 2, 'formustang.jpg'),
-('MB0014', NULL, 'Mercedes-Benz', 'E-class', '9500000', 'Available', 'Wagon', '2018', 5, 'mbeclass.jpg'),
-('MB0017', NULL, 'Mercedes-Benz', 'S-class', '20400000', 'Available', 'Sedan', '2018', 4, 'mbsclass.jpg'),
+('BMW002', NULL, 'BMW', '4-serie', '5305000', 'Available', 'Hatchback', '2021', 4, 'bmw4serie.jpg'),
+('BMW003', NULL, 'BMW', 'X7', '5400000', 'Available', 'SUV', '2021', 6, 'bmwx7.jpg'),
+('C001', NULL, 'Cadillac', 'CT6', '9000000', 'Available', 'Sedan', '2021', 4, 'cadillacct6.jpg'),
+('F001', NULL, 'Ferrari', 'LaFerrari', '200000000', 'Available', 'Coupe', '2021', 2, 'ferarilaferari.jpg'),
+('FR001', NULL, 'Ford', 'Mustang', '12000000', 'Available', 'Coupe', '2018', 2, 'formustang.jpg'),
+('MB001', NULL, 'Mercedes-Benz', 'E-class', '9500000', 'Available', 'Wagon', '2018', 5, 'mbeclass.jpg'),
 ('MB002', NULL, 'Mercedes-Benz', 'AMG GT', '5000000', 'Available', 'Coupe', '2020', 2, 'mbamggt.jpg'),
-('MB008', NULL, 'Mercedes-Benz', 'EQC', '3500000', 'Available', 'SUV', '2019', 5, 'mbeqc.jpg'),
-('PC0011', NULL, 'Porsche', 'Panamera', '20000000', 'Available', 'Hatchback', '2021', 4, 'pcoanamera.jpg'),
-('PC003', NULL, 'Porsche', 'Macan', '4000000', 'Available', 'SUV', '2018', 5, 'pcmacan.jpg');
+('MB003', NULL, 'Mercedes-Benz', 'S-class', '20400000', 'Available', 'Sedan', '2018', 4, 'mbsclass.jpg'),
+('MB004', NULL, 'Mercedes-Benz', 'EQC', '3500000', 'Available', 'SUV', '2019', 5, 'mbeqc.jpg'),
+('PC001', NULL, 'Porsche', 'Panamera', '20000000', 'Available', 'Hatchback', '2021', 4, 'pcoanamera.jpg'),
+('PC002', NULL, 'Porsche', 'Macan', '4000000', 'Available', 'SUV', '2018', 5, 'pcmacan.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contactus`
+--
+
+CREATE TABLE `contactus` (
+  `contactid` int(11) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contactus`
+--
+
+INSERT INTO `contactus` (`contactid`, `fullname`, `email`, `message`) VALUES
+(1, 'Tarik', '180204061@aust.edu', 'Hi');
 
 -- --------------------------------------------------------
 
@@ -142,26 +169,33 @@ INSERT INTO `cars` (`carId`, `carName`, `carBrand`, `carModel`, `carPrice`, `car
 --
 
 CREATE TABLE `installment` (
-  `insId` int(11) NOT NULL,
+  `insId` int(15) NOT NULL,
   `tId` varchar(50) DEFAULT NULL,
   `userEmail` varchar(100) DEFAULT NULL,
-  `totalinstallment` int(11) DEFAULT NULL,
-  `totalCost` int(11) DEFAULT NULL,
-  `paid` int(11) DEFAULT NULL
+  `paidInstallment` int(11) DEFAULT 1,
+  `totalCost` varchar(255) DEFAULT NULL,
+  `paid` varchar(255) DEFAULT NULL,
+  `installmentDate` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `mods`
+-- Table structure for table `service`
 --
 
-CREATE TABLE `mods` (
-  `modId` varchar(50) NOT NULL,
-  `modType` varchar(50) DEFAULT NULL,
-  `modModel` varchar(50) DEFAULT NULL,
-  `modPrice` int(11) DEFAULT NULL
+CREATE TABLE `service` (
+  `serviceId` int(11) NOT NULL,
+  `tId` varchar(255) NOT NULL,
+  `problemdescription` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `service`
+--
+
+INSERT INTO `service` (`serviceId`, `tId`, `problemdescription`) VALUES
+(18, '1647023143-M-1014-9989', 'car stop');
 
 -- --------------------------------------------------------
 
@@ -174,7 +208,10 @@ CREATE TABLE `transactions` (
   `tId` varchar(50) NOT NULL,
   `userEmail` varchar(100) DEFAULT NULL,
   `transType` varchar(100) NOT NULL,
-  `productId` varchar(50) DEFAULT NULL
+  `productId` varchar(50) DEFAULT NULL,
+  `accountNo` varchar(255) NOT NULL,
+  `unitPrice` varchar(50) NOT NULL,
+  `productPrice` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -232,16 +269,22 @@ ALTER TABLE `cars`
   ADD PRIMARY KEY (`carId`);
 
 --
+-- Indexes for table `contactus`
+--
+ALTER TABLE `contactus`
+  ADD PRIMARY KEY (`contactid`);
+
+--
 -- Indexes for table `installment`
 --
 ALTER TABLE `installment`
   ADD PRIMARY KEY (`insId`);
 
 --
--- Indexes for table `mods`
+-- Indexes for table `service`
 --
-ALTER TABLE `mods`
-  ADD PRIMARY KEY (`modId`);
+ALTER TABLE `service`
+  ADD PRIMARY KEY (`serviceId`);
 
 --
 -- Indexes for table `transactions`
@@ -264,7 +307,7 @@ ALTER TABLE `userinfo`
 -- AUTO_INCREMENT for table `cardetails`
 --
 ALTER TABLE `cardetails`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `cargallary`
@@ -273,16 +316,28 @@ ALTER TABLE `cargallary`
   MODIFY `piId` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `contactus`
+--
+ALTER TABLE `contactus`
+  MODIFY `contactid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `installment`
 --
 ALTER TABLE `installment`
-  MODIFY `insId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `insId` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `service`
+--
+ALTER TABLE `service`
+  MODIFY `serviceId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `purchaseId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `purchaseId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=265;
 
 --
 -- Constraints for dumped tables
